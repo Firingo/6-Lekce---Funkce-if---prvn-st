1e2a20be5cd80f706f7ef42978cfd5dd5215df4c
Firingo <petr.havlin.0605@gmail.com>
2022-07-28 12:36:15 +0200

    6 Lekce - Funkce if - první část

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Zadej nějaké číslo");
+int a = int.Parse(Console.ReadLine());
+if (a > 5) ;
+Console.WriteLine("Zadal jsi číslo větší než 5!");
+Console.WriteLine("Děkuji za zadání");
+Console.ReadKey();
